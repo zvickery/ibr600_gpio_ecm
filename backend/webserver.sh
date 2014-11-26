ed7b787fa36ed46a1e445a9a23d2fe1081dcac81
Zach Vickery <zvickery@gmail.com>
2014-11-26 14:46:48 -0700

    Switch to jquery mobile widget.  Changed logging to use real IP instead of proxied IP.

--- backend/webserver.sh
+++ backend/webserver.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gunicorn lights:app -D --error-logfile log/lights.error.log --access-logfile log/lights.access.log
+gunicorn lights:app -D --error-logfile log/lights.error.log --access-logfile log/lights.access.log --access-logformat '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
